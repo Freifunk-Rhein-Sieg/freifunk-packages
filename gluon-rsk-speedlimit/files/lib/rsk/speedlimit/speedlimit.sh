5fd06c7220239446e278b829c26b2617ad0c92ac
Claus Marxmeier <claus@marxmeier.de>
2017-08-15 19:47:40 +0200

    Update speedlimit.sh

--- gluon-rsk-speedlimit/files/lib/rsk/speedlimit/speedlimit.sh
+++ gluon-rsk-speedlimit/files/lib/rsk/speedlimit/speedlimit.sh
@@ -5,15 +5,16 @@
 
 ROLE=`uci get gluon-node-info.@system[0].role`
 
-OFF_HOUR=`uci get rsk.@speedlimit[0].houroff`
-ON_HOUR=`uci get rsk.@speedlimit[0].houron`
+LIMIT_HOUR=`uci get rsk.@speedlimit[0].hour_limit`
+STANDADR_HOUR=`uci get rsk.@speedlimit[0].hour_normal`
 DISABLED=`uci get rsk.@speedlimit[0].disabled`
 
 HOUR=`date +"%H"`
 if [ $DISABLED -eq 0 ]; then
 
-if [ $HOUR -ge $OFF_HOUR ] || [ $HOUR -lt $ON_HOUR ]; then
+if [ $HOUR -ge $LIMIT_HOUR ] || [ $HOUR -lt $STANDARD_HOUR ]; then
                 LIMIT_OFF=1
         else
                 LIMIT_OFF=0
 fi
+

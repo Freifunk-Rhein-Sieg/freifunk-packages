8780c70cece1fc95691ce4aa517cf2f368837be5
Claus Marxmeier <claus@marxmeier.de>
2017-08-15 19:53:49 +0200

    Update speedlimit.sh

--- gluon-rsk-speedlimit/files/lib/rsk/speedlimit/speedlimit.sh
+++ gluon-rsk-speedlimit/files/lib/rsk/speedlimit/speedlimit.sh
@@ -3,7 +3,10 @@
 /usr/sbin/ntpd -q -p 2.openwrt.pool.ntp.org             #Check Time before Run
 # nice try, but most often, time is not in sync - check manually ! 
 
-ROLE=`uci get gluon-node-info.@system[0].role`
+#
+# need to check node role?
+#
+#ROLE=`uci get gluon-node-info.@system[0].role`
 #
 # times to switch mesh vpn bandwith
 #
@@ -15,10 +18,12 @@ HOUR_STANDARD=`uci get rsk.@speedlimit[0].hour_normal`
 # default
 INGRESS_DEFAULT=`uci get rsk.@speedlimit[0].default_ingress`
 EGRESS_DEFAULT=`uci get rsk.@speedlimit[0].default_egress`
-INGRESS_LIMIT=`uci get rsk.@speedlimit[0].default_ingress`
 # limited
+INGRESS_LIMIT=`uci get rsk.@speedlimit[0].default_ingress`
 EGRESS_LIMIT=`uci get rsk.@speedlimit[0].default_egress`
 DISABLED=`uci get rsk.@speedlimit[0].disabled`
+#
+
 
 HOUR=`date +"%H"`
 if [ $DISABLED -eq 0 ]; then

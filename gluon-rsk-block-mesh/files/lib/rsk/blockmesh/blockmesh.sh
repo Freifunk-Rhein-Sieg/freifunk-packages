462fa36e3e53f484b014cb8e0ec06f1cbde3a5c0
Claus Marxmeier <claus@marxmeier.de>
2017-08-19 13:34:59 +0200

    Update blockmesh.sh

--- gluon-rsk-block-mesh/files/lib/rsk/blockmesh/blockmesh.sh
+++ gluon-rsk-block-mesh/files/lib/rsk/blockmesh/blockmesh.sh
@@ -23,6 +23,20 @@ if [ $DISABLED -eq 0 ]; then
     # command:  iw dev $MESH_IFACE station set $HW_ADDR plink_action [open|block]
     # see: https://forum.freifunk.net/t/unnoetige-mesh-verbindungen-mit-mac-filter-verhindern/13244/10
     #
+    # from iw --help:
+    #  dev <devname> station dump
+    #            List all stations known, e.g. the AP on managed interfaces
+    #    dev <devname> station set <MAC address> mesh_power_mode <active|light|deep>
+    #            Set link-specific mesh power mode for this station
+    #   dev <devname> station set <MAC address> vlan <ifindex>
+    #            Set an AP VLAN for this station.
+    #   dev <devname> station set <MAC address> plink_action <open|block>
+    #            Set mesh peer link action for this station (peer).
+    #   dev <devname> station del <MAC address>
+    #            Remove the given station entry (use with caution!)
+    #   dev <devname> station get <MAC address>
+    #            Get information for a specific station.
+  
     # 11s interface: mesh0
            LIST=$(echo $MAC_LIST | tr "\s" "\n")
             # loop for every entry in maclist

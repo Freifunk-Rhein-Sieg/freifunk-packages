581f48ed25b394aef011bfbfc49751c7b328d375
Claus Marxmeier <claus@marxmeier.de>
2018-11-14 16:43:23 +0100

    Update check_gw.sh

--- gluon-check-mesh/files/lib/rsk/gluon-check-mesh/check_gw.sh
+++ gluon-check-mesh/files/lib/rsk/gluon-check-mesh/check_gw.sh
@@ -51,6 +51,7 @@ MAC3GW=`uci get rsk.@checkgw[0].mac3gw`
         # echo "Bisher $failcount Fehler\n"
         # enable wifi in case someone pressed false button
         uci set wireless.client_radio0.disabled='0'
+        uci set wireless.mesh_radio0.disabled='0'
         uci commit wireless
         wifi
         # end in case false button

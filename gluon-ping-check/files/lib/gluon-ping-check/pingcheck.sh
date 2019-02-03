663b60d171e9326bdc0bfc0d6790f77b039d9629
Claus Marxmeier <claus@marxmeier.de>
2019-02-03 02:19:18 +0100

    Update pingcheck.sh

--- gluon-ping-check/files/lib/gluon-ping-check/pingcheck.sh
+++ gluon-ping-check/files/lib/gluon-ping-check/pingcheck.sh
@@ -13,13 +13,41 @@
 #
 IP_LIST=`uci get rsk.@pingcheck[0].iplist`
 DISABLED=`uci get rsk.@pingcheck[0].disabled`
+FAILCOUNTFILE=/var/run/ping_failcount
+# wie viele Fehlversuche vor dem Reboot (min)
+MAXFAILCOUNT=`uci get rsk.@pingcheck[0].maxfail`
 #
 #
 #
 if [ $DISABLED -eq 0 ]; then
 
 
+   if [ -f $FAILCOUNTFILE ]; then
+      read failcount < $FAILCOUNTFILE
+    
 
+      LIST=$(echo $IP_LIST | tr "\s" "\n")
+            # loop for every entry in iplist
+              # echo $LIST
+                for IP in $LIST
+                 do
+                   PING_DROP = `ping -c 1 -6 -q $IP | grep received | cut -d ',' -f 3 | cut -d '%' -f 1`
+                   if [ $PING_DROP -eq 0 ]; then
+                        # everything is ok
+                        else
+                        # we have drops - lets increase error count
+                        
+                   fi
+
+
+                 done
+
+    else
+        echo 0 > $FAILCOUNTFILE
+        # debug
+        # echo "Bisher keine Fehler\n"
+
+   fi
 
 else
         echo "pingcheck uci setting shows DISABLED=true"

1c1278b1c9b7e66b857489036cfd8b14b862120b
Claus Marxmeier <claus@marxmeier.de>
2025-07-07 17:19:20 +0200

    Update ssid-changer.sh

--- gluon-ssid-changer/files/lib/rsk/ssid-changer/ssid-changer.sh
+++ gluon-ssid-changer/files/lib/rsk/ssid-changer/ssid-changer.sh
@@ -49,7 +49,7 @@ then
                       read failcount < $FAILCOUNTFILE
                        # we have drops - lets increase error count
                         failcount=$(($failcount+1))
-                        # echo 'schreibe failcounter ins file'
+                        echo 'schreibe $failcount failcounter ins file'
                         echo $failcount > $FAILCOUNTFILE
                         if [ $failcount -ge $MAXFAILCOUNT ]; then
                           echo 0 > $FAILCOUNTFILE
